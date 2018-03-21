4eb80093ee3825c9a28ceeabc76e5c1e943b6993
Cedric Schoeni <cedricschoeni123@gmx.ch>
2018-03-21 20:30:58 +0100

    Unity Project added
    first commit

---
+++ NN_Cars_v1/Assets/Scripts/CarControl.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class CarControl : MonoBehaviour {
+
+    [SerializeField]
+    public bool run = true;
+    public float velocity;
+    public float left90;
+    public float left45;
+    public float fd;
+    public float right45;
+    public float right90;
+    private float accelerationChange = 0;
+    private float rotationChange = 0;
+    public DeepNeuralNetwork nn;
+
+    
+
+    LayerMask lm;
+
+    public int score = 0;
+    public bool active = true;
+
+    private int colliderIndex = 0;
+
+    // Use this for initialization
+    void Start () {
+        nn = new DeepNeuralNetwork(6, new int[] { 5, 4 }, 4);
+        lm = 1 << 8;
+    }
+
+    public void resetGame()
+    {
+        score = 0;
+        active = true;
+        accelerationChange = 0;
+        rotationChange = 0;
+        velocity = 0;
+        colliderIndex = 0;
+        resetPosition();
+    }
+
+    private void resetPosition()
+    {
+        transform.position = new Vector3(0, 0, 0);
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+    }
+
+    public void moveCar(int output)
+    {
+        switch (output)
+        {
+            // Turn Right
+            case 0:
+                rotationChange = 90;
+                accelerationChange = 0;
+                break;
+            // Turn Left
+            case 1:
+                rotationChange = -90;
+                accelerationChange = 0;
+                break;
+            // Accelerate
+            case 2:
+                rotationChange = 0;
+                accelerationChange = 0.1f;
+                break;
+            // Stop
+            case 3:
+                rotationChange = 0;
+                accelerationChange = -0.1f;
+                break;
+            default:
+                break;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (active)
+        {
+            // apply accelerationChange to velocity of the car
+            velocity += accelerationChange;
+
+            transform.Translate(velocity * Time.deltaTime, 0, 0);
+            transform.Rotate(0, rotationChange * Time.deltaTime, 0);
+
+            Vector3 left_90 = transform.TransformDirection(Vector3.forward);
+            Vector3 left_45 = transform.TransformDirection(Vector3.right - Vector3.back);
+            Vector3 forward = transform.TransformDirection(Vector3.right);
+            Vector3 right_45= transform.TransformDirection(Vector3.right - Vector3.forward);
+            Vector3 right_90 = transform.TransformDirection(Vector3.back);
+
+            Debug.DrawRay(transform.position, left_90 * 6, Color.red);
+            Debug.DrawRay(transform.position, left_45 * 4.243f, Color.yellow);
+            Debug.DrawRay(transform.position, forward * 6, Color.green);
+            Debug.DrawRay(transform.position, right_45 * 4.243f, Color.blue);
+            Debug.DrawRay(transform.position, right_90 * 6, Color.magenta);
+            RaycastHit hit;
+
+               
+            left90 = 6;
+            left45 = 6;
+            fd = 6;
+            right45 = 6;
+            right90 = 6;
+
+            if (Physics.Raycast(transform.position, left_90, out hit, 6, lm.value))
+                if (hit.transform.gameObject.name == "Cube")
+                {
+                    left90 = hit.distance - 0.5f;
+                }
+                    
+            if (Physics.Raycast(transform.position, left_45, out hit, 6, lm.value))
+                if (hit.transform.gameObject.name == "Cube")
+                {
+                    left45 = hit.distance - 0.707f;
+                }
+                    
+            if (Physics.Raycast(transform.position, forward, out hit, 6, lm.value))
+                if (hit.transform.gameObject.name == "Cube")
+                {
+                    fd = hit.distance - 1f;
+                }
+                    
+            if (Physics.Raycast(transform.position, right_45, out hit, 6, lm.value))
+                if (hit.transform.gameObject.name == "Cube")
+                {
+                    right45 = hit.distance - 0.707f;
+                }
+                    
+            if (Physics.Raycast(transform.position, right_90, out hit, 6, lm.value))
+                if (hit.transform.gameObject.name == "Cube")
+                {
+                    right90 = hit.distance - 0.5f;
+                }
+                    
+
+
+            double[] output = nn.ComputeOutputs(new double[] { left90, left45, fd, right45, right90, velocity });
+            double maxValue = output.Max();
+            int maxIndex = output.ToList().IndexOf(maxValue);
+            if (run) moveCar(maxIndex);
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.transform.gameObject.name == "Cube")
+        {
+            if (run) active = false;
+        }
+        /** old fitness function - not working properly */
+        if (col.transform.gameObject.name == CarController.colliders[colliderIndex])
+        {
+
+
+
+            float distance = Mathf.Max(Vector3.Distance(col.transform.position, transform.position), 1);
+            score += (int) (8 / distance + 5);
+            colliderIndex++;
+            if (colliderIndex == CarController.colliders.Length)
+            {
+                colliderIndex = 0;
+            }
+        }
+        
+}
+}
---
+++ NN_Cars_v1/Assets/Scripts/CarController.cs
@@ -0,0 +1,208 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class CarController : MonoBehaviour {
+
+    [SerializeField]
+    public bool run = true;
+    public float maxTime = 20;
+    public GameObject redCarPrefab;
+    public GameObject greenCarPrefab;
+    public GameObject blueCarPrefab;
+    public GameObject orangeCarPrefab;
+    public GameObject yellowCarPrefab;
+    public GameObject ScorepointParent;
+    public GameObject redCarParent;
+    public GameObject greenCarParent;
+    public GameObject blueCarParent;
+    public GameObject orangeCarParent;
+    public GameObject yellowCarParent;
+    public int redCarCount;
+    public int greenCarCount;
+    public int blueCarCount;
+    public int orangeCarCount;
+    public Text score;
+    public Text gen;
+
+    private float time;
+    private int generation;
+
+    public static string[] colliders;
+
+    
+    List<CarControl> cars;
+    List<CarControl> redCars;
+    List<CarControl> greenCars;
+    List<CarControl> blueCars;
+    List<CarControl> orangeCars;
+    CarControl yellowCar;
+
+    // Use this for initialization
+    void Start () {
+        int count = ScorepointParent.transform.childCount;
+        colliders = new string[count];
+        for (int i = 0; i < count; i++)
+        {
+            colliders[i] = ScorepointParent.transform.GetChild(i).gameObject.name;
+        }
+        if (run)
+        {
+            
+
+            cars = new List<CarControl>();
+            redCars = initCars(redCarPrefab, redCarParent, redCarCount);
+            greenCars = initCars(greenCarPrefab, greenCarParent, greenCarCount);
+            blueCars = initCars(blueCarPrefab, blueCarParent, blueCarCount);
+            orangeCars = initCars(orangeCarPrefab, orangeCarParent, orangeCarCount);
+            yellowCar = initCars(yellowCarPrefab, yellowCarParent, 1)[0];
+        }
+    }
+
+    public void resetAll()
+    {
+        for (int i = 0; i < redCars.Count; i++)
+        {
+            // generates a completely random set of weights
+            redCars[i].nn.InitializeWeights();
+        }
+
+        for (int i = 0; i < greenCars.Count; i++)
+        {
+            // generates a completely random set of weights
+            greenCars[i].nn.InitializeWeights();
+        }
+
+        for (int i = 0; i < blueCars.Count; i++)
+        {
+            // generates a completely random set of weights
+            blueCars[i].nn.InitializeWeights();
+        }
+
+        for (int i = 0; i < orangeCars.Count; i++)
+        {
+            // generates a completely random set of weights
+            orangeCars[i].nn.InitializeWeights();
+        }
+
+        yellowCar.nn.InitializeWeights();
+        gen.text = "Generation: 0";
+        score.text = "Score: 0";
+        generation = 0;
+        resetGame();
+    }
+
+    private List<CarControl> initCars(GameObject prefab, GameObject parent, int count)
+    {
+        List<CarControl> list = new List<CarControl>();
+        for (int i = 0; i < count; i++)
+        {
+            CarControl car = Instantiate(prefab, parent.transform).GetComponent<CarControl>();
+            list.Add(car);
+            cars.Add(car);
+        }
+        return list;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (run)
+        {
+            time += Time.deltaTime;
+
+            bool active = false;
+            for (int i = 0; i < cars.Count; i++)
+            {
+                if (cars[i].active)
+                {
+                    active = true;
+                    break;
+                }
+            }
+
+            if (!active || time > maxTime)
+            { // end of this generation
+                time = 0;
+                int highestIndex = -1;
+                int highestScore = 0;
+                for (int i = 0; i < cars.Count; i++)
+                {
+                    if (cars[i].score >= highestScore)
+                    {
+                        highestIndex = i;
+                        highestScore = cars[i].score;
+                    }
+                }
+
+                double[] bestWeights = cars[highestIndex].nn.GetWeights();
+
+
+                for (int i = 0; i < redCars.Count; i++)
+                {
+                    // generates a completely random set of weights
+                    redCars[i].nn.InitializeWeights();
+                }
+
+                for (int i = 0; i < greenCars.Count; i++)
+                {
+                    // update weights towards best weights scaled by a factor
+                    greenCars[i].nn.updateWeights(bestWeights, 80, 50);
+                }
+
+                for (int i = 0; i < blueCars.Count; i++)
+                {
+                    // update weights towards best weights scaled by a factor
+                    blueCars[i].nn.updateWeights(bestWeights, 90, 20);
+                }
+
+                for (int i = 0; i < orangeCars.Count; i++)
+                {
+                    // copy of yellow car but with small mutation chance
+                    orangeCars[i].nn.updateWeights(bestWeights, 100, 10);
+                }
+
+                // Yellow car will always have the best weights
+                yellowCar.nn.SetWeights(bestWeights);
+
+                //Debug.Log("Highest Score: " + highestScore + " Scored by " + cars[highestIndex].gameObject.name);
+
+                score.text = "Score: " + highestScore;
+                gen.text = "Generation: " + ++generation;
+                resetGame();
+            }
+        }
+    }
+
+    private void printWeights(string s, double[] wgs)
+    {
+        for (int i = 0; i < wgs.Length; i++)
+        {
+            s += " " + i + ": " + wgs[i];
+        }
+        Debug.Log(s);
+    }
+
+    private void resetGame()
+    {
+        for (int i = 0; i < cars.Count; i++)
+        {
+            cars[i].resetGame();
+        }
+    }
+
+    public void skipRound()
+    {
+        for (int i = 0; i < cars.Count; i++)
+        {
+            cars[i].active = false;
+        }
+    }
+
+    public void LoadScene(string number)
+    {
+        SceneManager.LoadScene("Scene_"+number);
+    }
+}
